e9aff00b98197f19d224470c5e3c6017f4010916
SimmonsPeter <0353384@cegepoutaouais.qc.ca>
2023-01-18 16:52:55 -0500

    Enfin debloqué, merci Fred et Sebastian

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-const panier = {
-    pommes : 2,
-    poires : 0,
-    prunes : 0
+let panier = {
+    pommes: 0,
+    poires: 0,
+    prunes: 0
 }
 
 // Pour afficher le nombre de pommes dans la facture détaillée
 document.querySelector("#qtePommes").textContent = panier.pommes;
+
+//Lié les bouttons à la fonction
+
+document.getElementById("BTNPommes").onclick = function(){Ajouter("Pommes")};
+document.getElementById("BTNPoires").onclick = function(){Ajouter("Poires")};
+document.getElementById("BTNPrunes").onclick = function(){Ajouter("Prunes")};
+
+function Ajouter(choix) {
+    switch (choix) {
+        case "Pommes":
+            panier.pommes++;
+            document.getElementById("qtePommes").innerText = panier.pommes;
+
+            break;
+    }
+}
